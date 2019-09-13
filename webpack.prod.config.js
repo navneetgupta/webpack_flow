43249c4ac58d0be546639e64b5f7c8a4938eb8b7
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-13 14:20:54 +0400

    Lecture 414. Creating the Production Workflow

---
+++ webpack.prod.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  devtool: "cheap-module-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: ""
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                // css-modules loading
+                localIdentName: "[name]__[local]__[hash:base64:5]"
+              }
+            }
+          }, // order is impt order goes from right to left or bottom to top so first css-loader willbe applied followed by style-loader
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [autoprefixer()]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: "url-loader?limit=8000&name=images/[name].[ext]" // limit in bytes
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+};
